34f7ce7432e928118c325d430dd2778ac0a3e684
awol2010ex <awol2010ex@gmail.com>
2017-05-22 15:29:38 +0800

    my ignite web manager :webconsole spring boot init

---
+++ web-console/src/main/kotlin/com/towery/myignite/WebConsole.kt
@@ -0,0 +1,28 @@
+package com.towery.myignite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+
+
+/**
+ * Created by User on 2017/5/22.
+ */
+
+
+@SpringBootApplication
+open class WebConsole : SpringBootServletInitializer() {
+
+    // Used when deploying to a standalone servlet container
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(WebConsole::class.java)
+    }
+
+    companion object {
+
+        @JvmStatic open fun main(args: Array<String>) {
+            SpringApplication.run(WebConsole::class.java, *args)
+        }
+    }
+}
