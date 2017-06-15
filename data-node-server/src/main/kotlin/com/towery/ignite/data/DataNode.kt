7b448a66dd79d6d8700915b94888dfcd46b37cc5
awol2010ex <awol2010ex@gmail.com>
2017-06-15 11:05:59 +0800

    data-node:query multithreads setting

--- data-node-server/src/main/kotlin/com/towery/ignite/data/DataNode.kt
+++ data-node-server/src/main/kotlin/com/towery/ignite/data/DataNode.kt
@@ -7,10 +7,11 @@ import com.towery.ignite.services.SqlDatamodelService
 import org.apache.ignite.Ignition
 import org.apache.ignite.cache.CacheMode
 import org.apache.ignite.cache.QueryEntity
+import org.apache.ignite.cache.QueryIndex
 import org.apache.ignite.configuration.CacheConfiguration
-import org.apache.ignite.configuration.CollectionConfiguration
 import org.apache.ignite.configuration.IgniteConfiguration
 import org.apache.ignite.configuration.MemoryConfiguration
+import org.apache.ignite.configuration.MemoryPolicyConfiguration
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi
 import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,8 +20,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import java.util.*
-import org.apache.ignite.configuration.MemoryPolicyConfiguration
-
 
 
 /** 服务启动执行
@@ -33,84 +32,91 @@ open class DataNode : CommandLineRunner {
     private val env: Environment? = null
     @Autowired
     val sqlDatamodelService: SqlDatamodelService? = null
-/*　启动脚本
-*   gradle bootRun --offline -PappArgs="['1']"
-* */
-    override  fun run(vararg args: String) {
+
+    /*　启动脚本
+    *   gradle bootRun --offline -PappArgs="['1']"
+    * */
+    override fun run(vararg args: String) {
         println(">>>>>>>>>>>>>>>data node启动执行，执行加载数据等操作 start <<<<<<<<<<<<<")
 
-        val modelId :String?= args[0];//模型ID
-        val modelVO :TSqlDatamodelVO?   =sqlDatamodelService!!.getSqlDatamodel(modelId!!)
+        val modelId: String? = args[0];//模型ID
+        val modelVO: TSqlDatamodelVO? = sqlDatamodelService!!.getSqlDatamodel(modelId!!)
 
-    // Create new configuration.
+        // Create new configuration.
         val cfg = IgniteConfiguration();
 
+        val memCfg = MemoryConfiguration()
 
+        val plc = MemoryPolicyConfiguration()
+
+        plc.name = "dfltPlc"
+        plc.initialSize = 4 * 1024L * 1024L * 1024L
+        plc.isMetricsEnabled = true
+
+        memCfg.setMemoryPolicies(plc)
+        memCfg.defaultMemoryPolicyName = "dfltPlc"
+
+
+
+        cfg.memoryConfiguration = memCfg
 
         val cacheCfg = CacheConfiguration<Any, Any>(modelVO!!.name)
-        val  sqlDatamodelItemList:List<TSqlDatamodelItemVO>  =sqlDatamodelService!!.getSqlDatamodelItemListByModelId(modelId!!)
+        val sqlDatamodelItemList: List<TSqlDatamodelItemVO> = sqlDatamodelService!!.getSqlDatamodelItemListByModelId(modelId!!)
 
-        var entitys =ArrayList<QueryEntity>()
-        for(itemVO :TSqlDatamodelItemVO  in sqlDatamodelItemList){
+        var entitys = ArrayList<QueryEntity>()
+
+        for (itemVO: TSqlDatamodelItemVO in sqlDatamodelItemList) {
             val entity = QueryEntity("CustomKey", itemVO.tablename)
-
+            var indexs =ArrayList<QueryIndex>()
             val flds = LinkedHashMap<String, String>()
             val keyFlds = HashSet<String>()
-            for( columnVO : TSqlDatamodelItemColumnVO in  itemVO.columnList!!){
-                flds.put( columnVO.columnname!! ,sqlDatamodelService!!.getDataTypeClassName(columnVO.columntype!!))
-                if(columnVO.pk== Integer(1)){
-                    keyFlds.add(columnVO.columnname!! );
+            for (columnVO: TSqlDatamodelItemColumnVO in itemVO.columnList!!) {
+                flds.put(columnVO.columnname!!, sqlDatamodelService!!.getDataTypeClassName(columnVO.columntype!!))
+                if (columnVO.pk == Integer(1)) {
+                    keyFlds.add(columnVO.columnname!!);
+                    indexs.add(QueryIndex(columnVO.columnname!!))
                 }
             }
             entity.fields = flds
-            if(keyFlds.size>0) {
+            if (keyFlds.size > 0) {
                 entity.setKeyFields(keyFlds)
             } else {
                 entity.setKeyType("java.lang.Long")
+
+                //indexs.add(QueryIndex("_key"))
             }
 
             // End of new settings, nothing else here is DML related
-            entity.setIndexes(Collections.emptyList());
+            entity.setIndexes(indexs);
             entitys.add(entity)
         }
-    cacheCfg.setQueryEntities(entitys);
+        cacheCfg.setQueryEntities(entitys);
 
-    cacheCfg.isCopyOnRead =(env!!.getProperty("ignite.copyOnRead","false")).toBoolean()
+        cacheCfg.isCopyOnRead = (env!!.getProperty("ignite.copyOnRead", "false")).toBoolean()
 
-    cacheCfg.isOnheapCacheEnabled =false
-    cacheCfg.cacheMode=CacheMode.PARTITIONED
-    cfg.setCacheConfiguration(cacheCfg);
+        cacheCfg.isOnheapCacheEnabled = false
+        cacheCfg.cacheMode = CacheMode.PARTITIONED
+        cacheCfg.memoryPolicyName ="dfltPlc"
+        cacheCfg.queryParallelism =32
+        cacheCfg.backups=0
 
-    cfg.isPeerClassLoadingEnabled=false
+        cfg.setCacheConfiguration(cacheCfg);
 
-
-    val ipFinder = TcpDiscoveryMulticastIpFinder();
-    ipFinder.setAddresses(Arrays.asList(env!!.getProperty("ignite.ipAddress","127.0.0.1:47500..47509")));
-    val tcpDiscoverySpi = TcpDiscoverySpi();
-    tcpDiscoverySpi.setIpFinder(ipFinder);
-    cfg.setDiscoverySpi(tcpDiscoverySpi);
-
-    cfg.publicThreadPoolSize =(env!!.getProperty("ignite.systemThreadPoolSize","64")).toInt()
-    cfg.systemThreadPoolSize=(env!!.getProperty("ignite.systemThreadPoolSize","64")).toInt()
+        cfg.isPeerClassLoadingEnabled = false
 
 
-    val memCfg = MemoryConfiguration()
+        val ipFinder = TcpDiscoveryMulticastIpFinder();
+        ipFinder.setAddresses(Arrays.asList(env!!.getProperty("ignite.ipAddress", "127.0.0.1:47500..47509")));
+        val tcpDiscoverySpi = TcpDiscoverySpi();
+        tcpDiscoverySpi.setIpFinder(ipFinder);
+        cfg.setDiscoverySpi(tcpDiscoverySpi);
 
-    val plc = MemoryPolicyConfiguration()
-
-    plc.name = "dfltPlc"
-    plc.initialSize  =800* 1024* 1024
-    plc.isMetricsEnabled=true
-
-    memCfg.setMemoryPolicies(plc)
-    memCfg.defaultMemoryPolicyName = "dfltPlc"
+        cfg.publicThreadPoolSize = (env!!.getProperty("ignite.systemThreadPoolSize", "64")).toInt()
+        cfg.systemThreadPoolSize = (env!!.getProperty("ignite.systemThreadPoolSize", "64")).toInt()
 
 
-
-    cfg.memoryConfiguration = memCfg
-
-    // Start Ignite node with given configuration.
-    Ignition.start(cfg);
+        // Start Ignite node with given configuration.
+        Ignition.start(cfg);
         println(">>>>>>>>>>>>>>>data node启动执行，执行加载数据等操作 end <<<<<<<<<<<<<")
     }
 }
